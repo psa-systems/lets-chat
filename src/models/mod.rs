9b2f1c74fa18d5c3c5646be87aeb0e47180294c2
longjacksonle <longjacksonle@gmail.com>
2026-04-14 22:43:43 +0200

    feat(reactions): add toggle_reaction and list_reactions db functions

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,6 +1,7 @@
 pub mod invite;
 pub mod message;
 pub mod mod_action;
+pub mod reaction;
 pub mod room;
 pub mod session;
 pub mod settings;
@@ -9,6 +10,7 @@ pub mod user;
 pub use invite::InviteCode;
 pub use message::Message;
 pub use mod_action::ModAction;
+pub use reaction::Reaction;
 pub use room::Room;
 pub use settings::SiteSettings;
 pub use user::User;
